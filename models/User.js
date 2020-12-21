426768e0faa553ec6a8221d8e1d8c49c057d0e4d
BaptisteVan1 <baptiste.vanlits@gmail.com>
2020-12-21 22:17:10 +0100

    added password hashing

--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 const {isEmail} = require('validator')
+const bcrypt = require('bcrypt')
 
 
 const userSchema = new mongoose.Schema({
@@ -17,6 +18,16 @@ const userSchema = new mongoose.Schema({
         minlength: [6, 'Minimun password length is 6 characters']
     },
 })
+
+
+// fire a function before doc saved to db
+userSchema.pre('save', async function(next){
+    const salt = await bcrypt.genSalt()
+    // this = instance of the user we're creating
+    this.password = await bcrypt.hash(this.password, salt)
+    next()
+})
+
 
 // creating model, name NEEDS to be singular of the db name
 const User = mongoose.model('user', userSchema)
