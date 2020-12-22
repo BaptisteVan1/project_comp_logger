7cd93f869d9c55f245245f648c84b0d1c7d9f4b4
BaptisteVan1 <baptiste.vanlits@gmail.com>
2020-12-22 20:58:33 +0100

    connected back and front end for signup and error handling

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,5 @@
 const User = require('../models/User')
+const jwt = require('jsonwebtoken')
 
 // handle errors
 const handleErrors = (err) => {
@@ -18,6 +19,15 @@ const handleErrors = (err) => {
     return errors;
   }
 
+  // value of 3 days in seconds
+const maxAge = 3*24*60*60
+
+const createToken = (id) => {
+    //secret to be changedand stored somewhere else. NOT SAFE
+    return jwt.sign({id}, 'my secret', {
+        expiresIn: maxAge
+    })
+}
 
 module.exports.signup_get = (req, res) => {
     res.render('signup')
@@ -32,7 +42,9 @@ module.exports.signup_post = async (req, res) => {
     // creating new user in DB
     try {
         const user = await User.create({email, password})
-        res.status(201).json(user)
+        const token = createToken(user._id)
+        res.cookie('jwt', token, {httpOnly: true, maxAge: maxAge * 1000})
+        res.status(201).json({user: user._id})
         console.log('New user created')
     } catch(err){
         const errors = handleErrors(err)
--- index.js
+++ index.js
@@ -2,13 +2,14 @@ const express = require('express')
 const mongoose = require('mongoose')
 const InitiateMongoServer = require('./config/db')
 const authRoutes = require('./routes/authRoutes')
-
+const cookieParser = require('cookie-parser')
 
 const app = express()
 
 // middleware
 app.use(express.static('public'))
 app.use(express.json())
+app.use(cookieParser())
 
 // port
 app.listen(5000, () => console.log('App listening on port 5000'))
